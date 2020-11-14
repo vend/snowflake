ca4110b3643593c1bba61815fa11611e0c415d7b
Evan Shaw <evan@vendhq.com>
2020-11-14 20:52:17 +1300

    Refactor PointsSummaries to function component

--- components/PointSummaries.tsx
+++ components/PointSummaries.tsx
@@ -6,84 +6,84 @@ interface Props {
   milestoneByTrack: MilestoneMap
 }
 
-class PointSummaries extends React.Component<Props> {
-  public render() {
-    const totalPoints = totalPointsFromMilestoneMap(this.props.milestoneByTrack)
+const PointSummaries: React.FunctionComponent<Props> = ({
+  milestoneByTrack,
+}) => {
+  const totalPoints = totalPointsFromMilestoneMap(milestoneByTrack)
 
-    let currentLevel
+  let currentLevel
 
-    let pointsForCurrentLevel = totalPoints
-    while (!(currentLevel = pointsToLevels[pointsForCurrentLevel])) {
-      pointsForCurrentLevel--
-    }
-
-    let pointsToNextLevel: number | string = 1
-    while (!pointsToLevels[totalPoints + pointsToNextLevel]) {
-      pointsToNextLevel++
-      if (pointsToNextLevel > 135) {
-        pointsToNextLevel = 'N/A'
-        break
-      }
-    }
-
-    const blocks = [
-      {
-        label: 'Current level',
-        value: currentLevel,
-      },
-      {
-        label: 'Total points',
-        value: totalPoints,
-      },
-      {
-        label: 'Points to next level',
-        value: pointsToNextLevel,
-      },
-    ]
-
-    return (
-      <table>
-        <style jsx>{`
-          table {
-            border-spacing: 3px;
-            margin-bottom: 20px;
-            margin-left: -3px;
-          }
-          .point-summary-label {
-            font-size: 12px;
-            text-align: center;
-            font-weight: normal;
-            width: 120px;
-          }
-          .point-summary-value {
-            width: 120px;
-            background: #eee;
-            font-size: 24px;
-            font-weight: bold;
-            line-height: 50px;
-            border-radius: 2px;
-            text-align: center;
-          }
-        `}</style>
-        <tbody>
-          <tr>
-            {blocks.map(({ label }, i) => (
-              <th key={i} className="point-summary-label">
-                {label}
-              </th>
-            ))}
-          </tr>
-          <tr>
-            {blocks.map(({ value }, i) => (
-              <td key={i} className="point-summary-value">
-                {value}
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
-    )
+  let pointsForCurrentLevel = totalPoints
+  while (!(currentLevel = pointsToLevels[pointsForCurrentLevel])) {
+    pointsForCurrentLevel--
   }
+
+  let pointsToNextLevel: number | string = 1
+  while (!pointsToLevels[totalPoints + pointsToNextLevel]) {
+    pointsToNextLevel++
+    if (pointsToNextLevel > 135) {
+      pointsToNextLevel = 'N/A'
+      break
+    }
+  }
+
+  const blocks = [
+    {
+      label: 'Current level',
+      value: currentLevel,
+    },
+    {
+      label: 'Total points',
+      value: totalPoints,
+    },
+    {
+      label: 'Points to next level',
+      value: pointsToNextLevel,
+    },
+  ]
+
+  return (
+    <table>
+      <style jsx>{`
+        table {
+          border-spacing: 3px;
+          margin-bottom: 20px;
+          margin-left: -3px;
+        }
+        .point-summary-label {
+          font-size: 12px;
+          text-align: center;
+          font-weight: normal;
+          width: 120px;
+        }
+        .point-summary-value {
+          width: 120px;
+          background: #eee;
+          font-size: 24px;
+          font-weight: bold;
+          line-height: 50px;
+          border-radius: 2px;
+          text-align: center;
+        }
+      `}</style>
+      <tbody>
+        <tr>
+          {blocks.map(({ label }, i) => (
+            <th key={i} className="point-summary-label">
+              {label}
+            </th>
+          ))}
+        </tr>
+        <tr>
+          {blocks.map(({ value }, i) => (
+            <td key={i} className="point-summary-value">
+              {value}
+            </td>
+          ))}
+        </tr>
+      </tbody>
+    </table>
+  )
 }
 
 export default PointSummaries
