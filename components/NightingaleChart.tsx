ac879b949e8eb91c322ab9b9114fc10e40281931
Evan Shaw <evan@vendhq.com>
2020-11-14 18:07:26 +1300

    Refactor NightingaleChart to function component

--- components/NightingaleChart.tsx
+++ components/NightingaleChart.tsx
@@ -12,123 +12,108 @@ interface Props {
   handleTrackMilestoneChangeFn: (trackId: TrackId, milestone: Milestone) => void
 }
 
-class NightingaleChart extends React.Component<Props> {
-  private colorScale: any
-  private radiusScale: any
-  private arcFn: any
+const radiusScale = d3
+  .scaleBand<number>()
+  .domain(arcMilestones)
+  .range([0.15 * width, 0.45 * width])
+  .paddingInner(0.1)
 
-  constructor(props: any) {
-    super(props)
+const arcFn = d3
+  .arc<number>()
+  .innerRadius(milestone => radiusScale(milestone)!)
+  .outerRadius(milestone => radiusScale(milestone)! + radiusScale.bandwidth())
+  .startAngle(-Math.PI / trackIds.length)
+  .endAngle(Math.PI / trackIds.length)
+  .padAngle(Math.PI / 200)
+  .padRadius(0.45 * width)
+  .cornerRadius(2)
 
-    this.colorScale = d3.scaleSequential(d3.interpolateWarm).domain([0, 5])
-
-    this.radiusScale = d3
-      .scaleBand<number>()
-      .domain(arcMilestones)
-      .range([0.15 * width, 0.45 * width])
-      .paddingInner(0.1)
-
-    this.arcFn = d3
-      .arc()
-      .innerRadius(milestone => this.radiusScale(milestone))
-      .outerRadius(
-        milestone => this.radiusScale(milestone) + this.radiusScale.bandwidth()
-      )
-      .startAngle(-Math.PI / trackIds.length)
-      .endAngle(Math.PI / trackIds.length)
-      .padAngle(Math.PI / 200)
-      .padRadius(0.45 * width)
-      .cornerRadius(2)
-  }
-
-  public render() {
-    const currentMilestoneId = this.props.milestoneByTrack[
-      this.props.focusedTrackId
-    ]
-    return (
-      <figure>
-        <style jsx>{`
-          figure {
-            margin: 0;
-          }
-          svg {
-            width: ${width}px;
-            height: ${width}px;
-          }
-          .track-milestone {
-            fill: #eee;
-            cursor: pointer;
-          }
-          .track-milestone-current,
-          .track-milestone:hover {
-            stroke: #000;
-            stroke-width: 4px;
-            stroke-linejoin: round;
-          }
-        `}</style>
-        <svg>
-          <g transform={`translate(${width / 2},${width / 2}) rotate(-33.75)`}>
-            {trackIds.map((trackId, i) => {
-              const isCurrentTrack = trackId === this.props.focusedTrackId
-              return (
-                <g
-                  key={trackId}
-                  transform={`rotate(${(i * 360) / trackIds.length})`}
-                >
-                  {arcMilestones.map(milestone => {
-                    const isCurrentMilestone =
-                      isCurrentTrack && milestone === currentMilestoneId
-                    const isMet =
-                      this.props.milestoneByTrack[trackId] >= milestone ||
-                      milestone === 0
-                    return (
-                      <path
-                        key={milestone}
-                        className={
-                          'track-milestone ' +
-                          (isMet ? 'is-met ' : ' ') +
-                          (isCurrentMilestone ? 'track-milestone-current' : '')
-                        }
-                        onClick={() =>
-                          this.props.handleTrackMilestoneChangeFn(
-                            trackId,
-                            milestone as Milestone
-                          )
-                        }
-                        d={this.arcFn(milestone)}
-                        style={{
-                          fill: isMet
-                            ? categoryColorScale(tracks[trackId].category)
-                            : undefined,
-                        }}
-                      />
-                    )
-                  })}
-                  <circle
-                    r="8"
-                    cx="0"
-                    cy="-50"
-                    style={{
-                      fill: categoryColorScale(tracks[trackId].category),
-                    }}
-                    className={
-                      'track-milestone ' +
-                      (isCurrentTrack && !currentMilestoneId
-                        ? 'track-milestone-current'
-                        : '')
-                    }
-                    onClick={() =>
-                      this.props.handleTrackMilestoneChangeFn(trackId, 0)
-                    }
-                  />
-                </g>
-              )
-            })}
-          </g>
-        </svg>
-      </figure>
-    )
-  }
+const NightingaleChart: React.FunctionComponent<Props> = ({
+  focusedTrackId,
+  milestoneByTrack,
+  handleTrackMilestoneChangeFn,
+}) => {
+  const currentMilestoneId = milestoneByTrack[focusedTrackId]
+  return (
+    <figure>
+      <style jsx>{`
+        figure {
+          margin: 0;
+        }
+        svg {
+          width: ${width}px;
+          height: ${width}px;
+        }
+        .track-milestone {
+          fill: #eee;
+          cursor: pointer;
+        }
+        .track-milestone-current,
+        .track-milestone:hover {
+          stroke: #000;
+          stroke-width: 4px;
+          stroke-linejoin: round;
+        }
+      `}</style>
+      <svg>
+        <g transform={`translate(${width / 2},${width / 2}) rotate(-33.75)`}>
+          {trackIds.map((trackId, i) => {
+            const isCurrentTrack = trackId === focusedTrackId
+            return (
+              <g
+                key={trackId}
+                transform={`rotate(${(i * 360) / trackIds.length})`}
+              >
+                {arcMilestones.map(milestone => {
+                  const isCurrentMilestone =
+                    isCurrentTrack && milestone === currentMilestoneId
+                  const isMet =
+                    milestoneByTrack[trackId] >= milestone || milestone === 0
+                  return (
+                    <path
+                      key={milestone}
+                      className={
+                        'track-milestone ' +
+                        (isMet ? 'is-met ' : ' ') +
+                        (isCurrentMilestone ? 'track-milestone-current' : '')
+                      }
+                      onClick={() =>
+                        handleTrackMilestoneChangeFn(
+                          trackId,
+                          milestone as Milestone
+                        )
+                      }
+                      d={arcFn(milestone)!}
+                      style={{
+                        fill: isMet
+                          ? categoryColorScale(tracks[trackId].category)
+                          : undefined,
+                      }}
+                    />
+                  )
+                })}
+                <circle
+                  r="8"
+                  cx="0"
+                  cy="-50"
+                  style={{
+                    fill: categoryColorScale(tracks[trackId].category),
+                  }}
+                  className={
+                    'track-milestone ' +
+                    (isCurrentTrack && !currentMilestoneId
+                      ? 'track-milestone-current'
+                      : '')
+                  }
+                  onClick={() => handleTrackMilestoneChangeFn(trackId, 0)}
+                />
+              </g>
+            )
+          })}
+        </g>
+      </svg>
+    </figure>
+  )
 }
 
 export default NightingaleChart
